61abfec746dd32ea9ebab47c10d2fc18d7a531f4
alexisandries <131961393+alexisandries@users.noreply.github.com>
2024-08-21 17:38:53 +0200

    Update DBox_mailgen.py

--- DBox_mailgen.py
+++ DBox_mailgen.py
@@ -486,7 +486,7 @@ def main():
         st.stop()
     st.sidebar.write("")
     st.sidebar.write("**IMPORTANT!**")
-    st.sidebar.write("Please contact me (Alexis 😊) if the responses frequently do not meet your needs. I can easily address this by providing the program with sample responses tailored to your specific use cases.")  
+    st.sidebar.write("Please contact me (Alexis 😊) if the responses frequently do not meet your needs. I may be able to address this by feeding the program with sample responses tailored to your specific use cases.")  
     st.sidebar.write("In general, please provide feedback on any areas or scenarios where the tool seems to fall short.")
     
     st.title("Multiagent AI Email System")
